32712d54d665a3805bdee94c0ce4f5d14660046f
paarth-sharma <paarthsharma2003@gmail.com>
2025-01-13 14:14:10 +0530

    fixed text visibility on home page in white mode

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -48,7 +48,7 @@ const TodoList = () => {
 			)}
 			{!isLoading && todos?.length === 0 && (
 				<Stack alignItems={"center"} gap='3'>
-					<Text fontSize={"xl"} textAlign={"center"} color={"gray.500"}>
+					<Text fontSize={"xl"} textAlign={"center"} color={"black.500"}>
 						All tasks completed! 🤞
 					</Text>
 					<img src='/go.png' alt='Go logo' width={70} height={70} />
